50b306c66c20e953fa60f6dedfffdd63626341b4
liuyingqun <liuyingqun@sensetime.com>
2021-02-26 19:00:19 +0800

    策略模式2

--- 16.策略模式.js
+++ 16.策略模式.js
@@ -12,7 +12,8 @@ var calculateBonusOld = function (performanceLevel, salary) {
 }
 
 // 策略模式的目的就是将算法的使用与算法的实现分离开来
-var strategy = {
+// 消除了原程序中大片的条件分支语句
+var strategy = { //策略对象
     "S": function (salary) {
         return salary * 4
     },
@@ -24,6 +25,7 @@ var strategy = {
     }
 }
 
+//Context并没有计算奖金的能力，而是把这个职责委托给了某个策略对象
 var calculateBonus = function (level, salary) {
     return strategy[level](salary)
 }
