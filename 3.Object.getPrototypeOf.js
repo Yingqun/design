83576e292de77fe1be2f1db989806522dd14eec8
liuyingqun <liuyingqun@zhongan.com>
2021-03-10 16:54:05 +0800

    虚拟代理合并http请求

--- 3.Object.getPrototypeOf.js
+++ 3.Object.getPrototypeOf.js
@@ -1,6 +1,6 @@
 var obj1 = new Object();
 var obj2 = {};
-
+// 方法返回指定对象的原型
 console.log(Object.getPrototypeOf(obj1))
 console.log(Object.getPrototypeOf(obj1) === Object.prototype)
 console.log(Object.getPrototypeOf(obj2) === Object.prototype) 
